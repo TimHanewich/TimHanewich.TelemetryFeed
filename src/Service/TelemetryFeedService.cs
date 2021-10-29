047ce508288fe88a69c924ceda9981b915a01789
Tim Hanewich <tahanewich@live.com>
2021-10-29 14:35:50 -0400

    Added TelemetryFeedService class

--- src/Service/TelemetryFeedService.cs
+++ src/Service/TelemetryFeedService.cs
@@ -1,7 +1,41 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TimHanewich.TelemetryFeed.Service
 {
-    
+    public class TelemetryFeedService
+    {
+        private Guid Key;
+
+        private async Task<string> ExecuteSqlAsync(string query) //The returned string is the response (in JSON probably)
+        {
+            HttpRequestMessage req = PrepareHttpRequestMessage();
+            req.Method = HttpMethod.Post;
+            req.RequestUri = new Uri("https://telemetryfeedapi.azurewebsites.net/api/sql");
+            req.Content = new StringContent(query);
+
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage resp = await hc.SendAsync(req);
+            string backback = await resp.Content.ReadAsStringAsync();
+
+            if (resp.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Request to server failed with code '" + resp.StatusCode.ToString() + "'. Msg: " + backback);
+            }
+
+            return backback;
+        }
+
+        private HttpRequestMessage PrepareHttpRequestMessage()
+        {
+            HttpRequestMessage req = new HttpRequestMessage();
+            req.Headers.Add("key", Key.ToString());
+            return req;
+        }
+    }
 }
