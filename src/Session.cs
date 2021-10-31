6987809a5da161c8907ebe00b3360f670fe2cbb4
Tim Hanewich <tahanewich@live.com>
2021-10-30 21:03:43 -0400

    Added columns to Session

--- src/Session.cs
+++ src/Session.cs
@@ -6,5 +6,9 @@ namespace TimHanewich.TelemetryFeed
     {
         public Guid Id {get; set;}
         public Guid Owner {get; set;}
+        public string Title {get; set;}
+        public DateTime CreatedAtUtc {get; set;}
+        public Guid RightLeanCalibration {get; set;}
+        public Guid LeftLeanCalibration {get; set;}
     }
 }
