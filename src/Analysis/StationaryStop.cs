2ee24227f357552387d56539561d50400bff2645
Tim Hanewich <tahanewich@live.com>
2021-11-09 22:02:08 -0500

    Duration

--- src/Analysis/StationaryStop.cs
+++ src/Analysis/StationaryStop.cs
@@ -11,5 +11,28 @@ namespace TimHanewich.TelemetryFeed.Analysis
         //Location
         public float Latitude {get; set;}
         public float Longitude {get; set;}
+
+        public TimeSpan Duration(TelemetrySnapshot[] all_packets)
+        {
+            TelemetrySnapshot b = null;
+            TelemetrySnapshot e = null;
+            foreach (TelemetrySnapshot ts in all_packets)
+            {
+                if (ts.Id == Beginning)
+                {
+                    b = ts;
+                }
+                else if (ts.Id == End)
+                {
+                    e = ts;
+                }
+            }
+            if (b == null || e == null)
+            {
+                throw new Exception("Unable to complete duration measurement. Unable to find both beginning and end snapshot in provided array.");
+            }
+            TimeSpan ToReturn = e.CapturedAtUtc - b.CapturedAtUtc;
+            return ToReturn;
+        }
     }
 }
