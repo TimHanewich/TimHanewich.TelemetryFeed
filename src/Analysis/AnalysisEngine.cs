084a89aff363ddb1909791ecf24eb9188b347015
Tim Hanewich <tahanewich@live.com>
2021-11-11 16:44:41 -0500

    Starting to implement buffer

--- src/Analysis/AnalysisEngine.cs
+++ src/Analysis/AnalysisEngine.cs
@@ -8,8 +8,9 @@ namespace TimHanewich.TelemetryFeed.Analysis
 {
     public class AnalysisEngine
     {
-        //LAST RECEIVED
-        private TelemetrySnapshot LastReceivedTelemetrySnapshot;
+        //LAST RECEIVED 
+        private TimeSpan BufferGuidelines;
+        private List<TelemetrySnapshot> LastReceivedTelemetrySnapshots;
 
         //for stops
         private TelemetrySnapshot ZeroDistanceCoveredFirstNoticed;
@@ -20,87 +21,53 @@ namespace TimHanewich.TelemetryFeed.Analysis
         private float _TopSpeedMph;
         private Guid _TopSpeedDetectedAt; //Guid of telemetry snapshot where the top speed was detected
 
+        public AnalysisEngine()
+        {
+            LastReceivedTelemetrySnapshots = new List<TelemetrySnapshot>();
+            BufferGuidelines = new TimeSpan(0, 0, 7);
+        }
 
         public void Feed(TelemetrySnapshot ts)
         {
-            if (LastReceivedTelemetrySnapshot != null)
-            {
-                if (ts.Latitude.HasValue && ts.Longitude.HasValue && LastReceivedTelemetrySnapshot.Latitude.HasValue && LastReceivedTelemetrySnapshot.Longitude.HasValue)
-                {
-                    //Measure distance
-                    Geolocation loc1 = new Geolocation();
-                    loc1.Latitude = LastReceivedTelemetrySnapshot.Latitude.Value;
-                    loc1.Longitude = LastReceivedTelemetrySnapshot.Longitude.Value;
-                    Geolocation loc2 = new Geolocation();
-                    loc2.Latitude = ts.Latitude.Value;
-                    loc2.Longitude = ts.Longitude.Value;
-                    Distance d = GeoToolkit.MeasureDistance(loc1, loc2);
-                    
-                    //Calculate stops
-                    if (d.Miles == 0)
-                    {
-                        if (_Status == RiderStatus.Moving)
-                        {
-                            //Only officially make this stationary (flip to stationary) if the stop was more than a certain length of time
-                            if (ZeroDistanceCoveredFirstNoticed == null)
-                            {
-                                ZeroDistanceCoveredFirstNoticed = ts;
-                            }
-                            else
-                            {
-                                TimeSpan TimeSinceFirstNoMove = ts.CapturedAtUtc - ZeroDistanceCoveredFirstNoticed.CapturedAtUtc;
-                                if (TimeSinceFirstNoMove.TotalSeconds > 4)
-                                {
-                                    _Status = RiderStatus.Stationary;
-                                }
-                            }
-                        }
-                        else if (_Status == RiderStatus.Stationary)
-                        {
-                            //Do nothing (wait until we start moving again)
-                        }
-                    }
-                    else if (d.Miles > 0)
-                    {
-                        if (_Status == RiderStatus.Stationary) //If this was marked as stationary, need to update it
-                        {
-                            _Status = RiderStatus.Moving; //Change to moving
-                            if (ZeroDistanceCoveredFirstNoticed != null)
-                            {
-                                StationaryStop ss = new StationaryStop();
-                                ss.Beginning = ZeroDistanceCoveredFirstNoticed.Id;
-                                ss.End = ts.Id;
-                                ss.Latitude = ZeroDistanceCoveredFirstNoticed.Latitude.Value;
-                                ss.Longitude = ZeroDistanceCoveredFirstNoticed.Longitude.Value;
-                                _Stops.Add(ss);
-
-                                ZeroDistanceCoveredFirstNoticed = null; //Set the zero distance covered to null.
-                            }
-                        }
-                        ZeroDistanceCoveredFirstNoticed = null;
-                    }
-
-                    //Calculate this speed
-                    if (d.Miles > 0)
-                    {
-                        TimeSpan timelapsed = ts.CapturedAtUtc - LastReceivedTelemetrySnapshot.CapturedAtUtc;
-                        if (timelapsed.TotalHours > 0)
-                        {
-                            float mph = d.Miles / Convert.ToSingle(timelapsed.TotalHours);
-                            if (mph > _TopSpeedMph)
-                            {
-                                _TopSpeedMph = mph;
-                                _TopSpeedDetectedAt = ts.Id;
-                            }
-                        }
-                    }
-
-                }
-            }
-            
-            LastReceivedTelemetrySnapshot = ts;
+            //Add it to the buffer
+            AddSnapshotToBuffer(ts);
         }
 
+        private void AddSnapshotToBuffer(TelemetrySnapshot ts)
+        {
+            if (LastReceivedTelemetrySnapshots == null)
+            {
+                LastReceivedTelemetrySnapshots = new List<TelemetrySnapshot>();
+            }
+            LastReceivedTelemetrySnapshots.Add(ts);
+
+            //Find the most recent one
+            TelemetrySnapshot most_recent = LastReceivedTelemetrySnapshots[0];
+            foreach (TelemetrySnapshot snap in LastReceivedTelemetrySnapshots)
+            {
+                if (snap.CapturedAtUtc > most_recent.CapturedAtUtc)
+                {
+                    most_recent = snap;
+                }
+            }
+
+            //Take out any that need to go
+            List<TelemetrySnapshot> ToRemove = new List<TelemetrySnapshot>();
+            foreach (TelemetrySnapshot snap in LastReceivedTelemetrySnapshots)
+            {
+                TimeSpan time_since = most_recent.CapturedAtUtc - snap.CapturedAtUtc;
+                if (time_since > BufferGuidelines)
+                {
+                    ToRemove.Add(snap);
+                }
+            }
+
+            //Take them out
+            foreach (TelemetrySnapshot snapshot in ToRemove)
+            {
+                LastReceivedTelemetrySnapshots.Remove(snapshot);
+            }
+        }
 
         public StationaryStop[] Stops
         {
