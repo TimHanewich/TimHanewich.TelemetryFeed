5200cf67b9324a4aa659741f8f72662448cde1b5
Tim Hanewich <tahanewich@live.com>
2021-11-11 12:35:28 -0500

    Removed multi parse

--- src/TelemetryFeedToolkit.cs
+++ src/TelemetryFeedToolkit.cs
@@ -18,33 +18,6 @@ namespace TimHanewich.TelemetryFeed
             return ToReturn.ToArray();
         }
 
-        public static TelemetrySnapshot[] ToTelemetrySnapshots(byte[] bytes)
-        {
-            int SingleTelemetrySnapshotLength = 100; //The length (in bytes) of a single telemetry snapshot
-            float NumberOfSnapshotsF = Convert.ToSingle(bytes.Length) / Convert.ToSingle(SingleTelemetrySnapshotLength);
-
-            //Get number of snapshots
-            int NumberOfSnapshots = 0;
-            try
-            {
-                NumberOfSnapshots = Convert.ToInt32(NumberOfSnapshotsF);
-            }
-            catch
-            {
-                throw new Exception("The number of bytes supplied doesn't make an even number of Telemetry Snapshots");
-            }
-
-            //Get each
-            ByteArrayManager bam = new ByteArrayManager(bytes);
-            List<TelemetrySnapshot> ToReturn = new List<TelemetrySnapshot>();
-            for (int t = 0; t < NumberOfSnapshots; t++)
-            {
-                byte[] thesebytes = bam.NextBytes(SingleTelemetrySnapshotLength);
-                TelemetrySnapshot ts = TelemetrySnapshot.FromBytes(thesebytes);
-                ToReturn.Add(ts);
-            }
-
-            return ToReturn.ToArray();
-        }
+        
     }
 }
