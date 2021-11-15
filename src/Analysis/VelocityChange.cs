2be29e2f13c000d1289ef043ee7d5c4647605604
Tim Hanewich <tahanewich@live.com>
2021-11-15 16:00:25 -0500

    Added properties to velocity change

--- src/Analysis/VelocityChange.cs
+++ src/Analysis/VelocityChange.cs
@@ -14,5 +14,32 @@ namespace TimHanewich.TelemetryFeed.Analysis
         public Guid EndingSnapshot {get; set;}
         public float BeginningSpeedMetersPerSecond {get; set;}
         public float EndingSpeedMetersPerSecond {get; set;}
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                return EndingUtc - BeginningUtc;
+            }
+        }
+
+        public AccelerationStatus VelocityChangeType
+        {
+            get
+            {
+                if (EndingSpeedMetersPerSecond > BeginningSpeedMetersPerSecond)
+                {
+                    return AccelerationStatus.Accelerating;
+                }
+                else if (EndingSpeedMetersPerSecond < BeginningSpeedMetersPerSecond)
+                {
+                    return AccelerationStatus.Decelerating;
+                }
+                else //This should never happen because a velocity change where the ending velocity is the same as the beginning velocity is invalid.
+                {
+                    return AccelerationStatus.MaintainingSpeed;
+                }
+            }
+        }
     }
 }
