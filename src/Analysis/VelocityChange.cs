1e828a1290c89ed86376561e01ea86a2dcb0e957
Tim Hanewich <tahanewich@live.com>
2021-11-15 16:11:56 -0500

    Added method for sorting velocity changes

--- src/Analysis/VelocityChange.cs
+++ src/Analysis/VelocityChange.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using TimHanewich.TelemetryFeed;
 using TimHanewich.TelemetryFeed.Analysis;
+using System.Collections.Generic;
 
 namespace TimHanewich.TelemetryFeed.Analysis
 {
@@ -52,5 +53,49 @@ namespace TimHanewich.TelemetryFeed.Analysis
                 return ToReturn;
             }
         }
+    
+    
+        #region "Toolkit"
+
+        //Sorts from highest velocity to lowest velocity change
+        public static VelocityChange[] Sort(VelocityChange[] changes)
+        {
+            if (changes == null)
+            {
+                return new VelocityChange[]{};
+            }
+            else if (changes.Length == 0)
+            {
+                return new VelocityChange[]{};
+            }
+            else if (changes.Length == 1)
+            {
+                return new VelocityChange[]{changes[0]};
+            }
+
+            List<VelocityChange> ToPullFrom = new List<VelocityChange>();
+            ToPullFrom.AddRange(changes);
+            List<VelocityChange> ToReturn = new List<VelocityChange>();
+            while (ToPullFrom.Count > 0)
+            {
+                VelocityChange winner = ToPullFrom[0];
+                foreach (VelocityChange vc in ToPullFrom)
+                {
+                    if (vc.AverageVelocityChangeMPS2 > winner.AverageVelocityChangeMPS2)
+                    {
+                        winner = vc;
+                    }
+                }
+
+                ToReturn.Add(winner);
+                ToPullFrom.Remove(winner);
+            }
+
+            return ToReturn.ToArray();
+        }
+
+
+        #endregion
+    
     }
 }
